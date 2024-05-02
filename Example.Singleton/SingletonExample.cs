6bb0329f342463f4adc6719cecc55677d4e1610c
Ivan Jones <ivanjo@vsp.com>
2024-05-02 14:50:17 -0700

    Adding singleton example

---
+++ Example.Singleton/SingletonExample.cs
@@ -0,0 +1,27 @@
+﻿// ReSharper disable InconsistentNaming
+namespace Example.Singleton;
+
+public class SingletonExample
+{
+
+    private static readonly object padlock = default!;
+    private static SingletonExample? instance;
+
+    private SingletonExample()
+    {
+
+    }
+
+    public static SingletonExample Instance
+    {
+        get
+        {
+            lock (padlock)
+            {
+                instance ??= new SingletonExample();
+            }
+            return instance;
+        }
+    }
+
+}
